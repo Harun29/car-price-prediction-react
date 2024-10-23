b5c2dedaae87bf22204642a53e7449ac843da4c4
Harun29 <ibrahimovic.harun01@gmail.com>
2024-10-23 22:34:40 +0200

    debugging prints in get prediction pandas context

--- src/Context/PandasContext.jsx
+++ src/Context/PandasContext.jsx
@@ -7,9 +7,9 @@ export function usePandas() {
 }
 
 export function PandasProvider({ children }) {
+
   const getPrediction = async (data) => {
     const url = "https://ml-flask-server-production.up.railway.app/get_prediction";
-
     try {
       const response = await fetch(url, {
         method: "POST",
@@ -20,18 +20,20 @@ export function PandasProvider({ children }) {
       });
 
       if (!response.ok) {
+        // Log response before throwing error
+        const errorText = await response.text();
+        console.error("Error Response:", errorText);
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
       const result = await response.json();
-      console.log("predition result: ", result)
-      // console.log("Prediction:", result.prediction);
-      // console.log("Filtered Vehicles:", result.vehicles);
+      console.log("Prediction result: ", result);
       return result;
     } catch (error) {
       console.error("Error:", error);
     }
   };
+
 
   const value = {
     getPrediction,
